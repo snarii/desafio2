543230fc3bb6162ce3310147fc990e5cd8da5830
snarii <100108863+snarii@users.noreply.github.com>
2022-03-05 02:29:13 -0300

    Update Questao2.py

--- Questao2.py
+++ Questao2.py
@@ -1,8 +1,8 @@
 a=""
 while (a!="sair"):
     print("Olá! :)")
-    a = input("Digite uma lista para obter os pares com a diferença x:")
-    x = int(input("Agora, digite o valor de x: "))
+    a = input("Digite uma lista para obter os pares com a diferença x:") #Determinação da lsta para criação dos pares.
+    x = int(input("Agora, digite o valor de x: ")) #Determinação do valor x (Resultado da subtração dos pares a partir da lista inserida)
     arr = []
     for i in a:
         if i.isdigit():
@@ -11,6 +11,6 @@ while (a!="sair"):
     arr2 = []
     for i in arr:
         for j in arr:
-            if (j - i == x):
+            if (j - i == x): #Condição da subtração igual ao valor x inserido.
                 arr2.append([j,i])
     print(len(arr2))
